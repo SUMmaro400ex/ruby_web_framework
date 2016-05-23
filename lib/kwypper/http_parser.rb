ea609d210fabc268f1871574c9c673a6a3130b08
Sean Sellek <me@seansellek.com>
2016-05-23 13:07:47 -0400

    Namespaced

--- lib/kwypper/http_parser.rb
+++ lib/kwypper/http_parser.rb
@@ -1,46 +1,48 @@
-class EmptyRequestError < RuntimeError; end
+module Kwypper
+  class EmptyRequestError < RuntimeError; end
 
-class HttpParser
-  def parse request_socket
-    @first_line = request_socket.gets or raise EmptyRequestError
-    Request.new do |r|
-      r.http_method = @first_line.split.first
-      r.path = parse_path
-      r.headers = parse_headers request_socket
-      r.cookies = parse_cookies r.headers["COOKIE"]
-      r.content_length = r.headers['CONTENT-LENGTH'].to_i
+  class HttpParser
+    def parse request_socket
+      @first_line = request_socket.gets or raise EmptyRequestError
+      Request.new do |r|
+        r.http_method = @first_line.split.first
+        r.path = parse_path
+        r.headers = parse_headers request_socket
+        r.cookies = parse_cookies r.headers["COOKIE"]
+        r.content_length = r.headers['CONTENT-LENGTH'].to_i
 
-      if r.content_length > 0
-        r.post_data = read_body request_socket, r.content_length
+        if r.content_length > 0
+          r.post_data = read_body request_socket, r.content_length
+        end
       end
     end
-  end
 
-  def parse_path
-    @first_line.split(' ')[1].split('?')[0]
-  end
+    def parse_path
+      @first_line.split(' ')[1].split('?')[0]
+    end
 
-  def parse_headers(raw_request)
-     lines = []
+    def parse_headers(raw_request)
+       lines = []
 
-     while (line = raw_request.gets) != "\r\n"
-       lines << line.chomp
+       while (line = raw_request.gets) != "\r\n"
+         lines << line.chomp
+       end
+
+       lines.each_with_object({}) do |line, request_headers|
+         key, val = line.split(/:\s/)
+         key = key.to_s.upcase.gsub '-', '_'
+         request_headers[key] = val.to_s.chomp
+       end
      end
 
-     lines.each_with_object({}) do |line, request_headers|
-       key, val = line.split(/:\s/)
-       key = key.to_s.upcase.gsub '-', '_'
-       request_headers[key] = val.to_s.chomp
+     def parse_cookies(cookie_value)
+       pairs = cookie_value.to_s.split(/;\s?/)
+       tuples = pairs.map { |c| c.split '=' }
+       Hash[tuples]
      end
-   end
 
-   def parse_cookies(cookie_value)
-     pairs = cookie_value.to_s.split(/;\s?/)
-     tuples = pairs.map { |c| c.split '=' }
-     Hash[tuples]
-   end
-
-   def read_body raw_request, content_length
-     Rack::Utils.parse_nested_query(raw_request.read(content_length))
-   end
+     def read_body raw_request, content_length
+       Rack::Utils.parse_nested_query(raw_request.read(content_length))
+     end
+  end
 end
--- lib/kwypper/http_server.rb
+++ lib/kwypper/http_server.rb
@@ -1,13 +1,15 @@
-class HttpServer < TCPServer
-  def serve
-    parser = HttpParser.new
+module Kwypper
+  class HttpServer < TCPServer
+    def serve
+      parser = HttpParser.new
 
-    while socket = accept
-      request = parser.parse socket
-      response = "Parsed request: #{request.inspect}"
+      while socket = accept
+        request = parser.parse socket
+        response = "Parsed request: #{request.inspect}"
 
-      socket.write response
-      socket.close
+        socket.write response
+        socket.close
+      end
     end
   end
 end
